d79ded3c7cca6f28fb66d55464562eba2a5e39de
mildlyinterestingdatascience <chris.ashburner@gmail.com>
2015-05-18 20:18:59 +1200

    Added initial draft for create_visual_banner post

---
+++ create_visual_banner/create_visual_banner.R
@@ -0,0 +1,15 @@
+# Creating a Mildly Interesting Data Science Banner
+# 2015-05-18
+# Chris Ashburner
+
+# Load Required Packages
+
+require(WDI)
+
+# Load Data
+
+# Transform Data
+
+# Explore Data
+
+# Final Visualisation
